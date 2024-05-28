78b68269fe3556abf129c8b9212c1b8e933077c7
TrashHat <jcdolojan@up.edu.ph>
2024-05-29 03:46:08 +0800

    back to square one

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,2 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE
-
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div> -->
-
-<script>
-	import dataset from '$lib/images/dataset.png';
-	import guidetodata from '$lib/images/guidetodata.png';
-</script>
-
-<!-- <img alt="The Dataset" src={dataset} /> -->
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,48 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess()],
+	preprocess: vitePreprocess(),
 
-	vitePlugin: {
-		inspector: true
-	},
-	// kit: {
-	// 	// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-	// 	// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-	// 	// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-	// 	adapter: adapter()
-	// }
-	// kit: {
-	// 	adapter: adapter(),
-	// 	paths: {
-	// 		base: process.env.NODE_ENV === 'production' ? '/new-website-132' : ''
-	// 	}
-	// }
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
-export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
-};
+export default config;
